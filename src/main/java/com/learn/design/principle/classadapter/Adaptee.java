f96dbcbe35a4e034a55776d5bd5f1cfa65654efa
Fritz <986431530@qq.com>
2021-10-18 11:04:37 +0800

    阶段提交

--- src/main/java/com/learn/design/principle/classadapter/Adaptee.java
+++ src/main/java/com/learn/design/principle/classadapter/Adaptee.java
@@ -14,6 +14,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Adaptee {
     public void adapteeRequest() {
-        log.info("被适配者的方法看看我能提交不111222");
+        log.info("被适配者的方法");
     }
 }
